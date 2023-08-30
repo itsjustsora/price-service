4779cd3cd4a4fba1dc310cc091d37a0a370bbf8b
keemsora <itsyoursora@gmail.com>
2023-08-30 17:08:39 +0900

    feat: update get zset value api

--- src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -3,11 +3,11 @@ package com.example.pricecompareredis.controller;
 import java.util.Set;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pricecompareredis.service.LowestPriceService;
-import com.example.pricecompareredis.service.LowestPriceServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,8 +18,8 @@ public class LowestPriceController {
 
 	private final LowestPriceService lowestPriceService;
 
-	@GetMapping("/product")
-	public Set GetZsetValue(String key) {
+	@GetMapping("/getZsetValue/{key}")
+	public Set GetZsetValue(@PathVariable String key) {
 		return lowestPriceService.getZsetValue(key);
 	}
 }
--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,15 +1,18 @@
 package com.example.pricecompareredis.service;
 
-import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LowestPriceServiceImpl implements LowestPriceService{
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
 	public Set getZsetValue(String key) {
-		Set myTempSet = new HashSet();
-		return myTempSet;
+		return redisTemplate.opsForZSet().rangeWithScores(key, 0, 9);
 	}
 }
